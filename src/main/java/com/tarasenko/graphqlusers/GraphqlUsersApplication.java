64696413c0f0520f9dc3b81be2b9d18ff9fd870c
Tarasenko <wjatscheslav47@gmail.com>
2020-01-05 16:28:06 +0200

    Project created

---
+++ src/main/java/com/tarasenko/graphqlusers/GraphqlUsersApplication.java
@@ -0,0 +1,15 @@
+package com.tarasenko.graphqlusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlUsersApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(GraphqlUsersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tarasenko/graphqlusers/GraphqlUsersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tarasenko.graphqlusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlUsersApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
